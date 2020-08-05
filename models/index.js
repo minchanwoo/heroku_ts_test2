7cadc870dedd5858544d81a34fd1cf6216edac9f
minchanwoo <dearmcw0070@gmail.com>
2020-08-06 04:04:39 +0900

    clearDB에 연결해보기

---
+++ models/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _sequelize = require("./sequelize");
+
+var _user = _interopRequireDefault(require("./user"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = {
+  sequelize: _sequelize.sequelize,
+  User: _user["default"]
+};
+exports["default"] = _default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9tb2RlbHMvaW5kZXgudHMiXSwibmFtZXMiOlsic2VxdWVsaXplIiwiVXNlciJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBOztBQUNBOzs7O2VBRWU7QUFDYkEsRUFBQUEsU0FBUyxFQUFUQSxvQkFEYTtBQUViQyxFQUFBQSxJQUFJLEVBQUpBO0FBRmEsQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IHNlcXVlbGl6ZSB9IGZyb20gJy4vc2VxdWVsaXplJztcbmltcG9ydCBVc2VyIGZyb20gJy4vdXNlcic7XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgc2VxdWVsaXplLFxuICBVc2VyXG59OyJdfQ==
